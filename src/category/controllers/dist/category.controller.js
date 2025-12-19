ef42cdbcc1086170716a5b53863c20bf3497cda8
Waqar Hussain <waqar.hussain@tacticminds.com>
2025-12-20 00:09:16 +0500

    fixd all mentioned issue

--- src/category/controllers/dist/category.controller.js
+++ src/category/controllers/dist/category.controller.js
@@ -170,7 +170,7 @@ var CategoryController = /** @class */ (function () {
             description: response_messages_enum_1.RESPONSE_MESSAGES.USER_CATEGORIES_LISTING
         }),
         common_1.UseGuards(user_auth_guard_1.UserAuthGuard),
-        common_1.Post('/wishlist/toggle'),
+        common_1.Put('/wishlist/toggle'),
         __param(0, common_1.Body()), __param(1, user_decorator_1.user())
     ], CategoryController.prototype, "userWishlistToggle");
     CategoryController = __decorate([
