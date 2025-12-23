a683b5ebc9211cc5a1bd5f4222e5e7bb6dd25b03
mAsadSiddique <asadsiddique.a@gmail.com>
2025-12-23 22:43:00 +0500

    fix: remove the ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ENV } from './config/constant';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
@@ -13,29 +13,34 @@ import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
-		TypeOrmModule.forRoot({
-			type: ENV.DB.TYPE as any,
-			host: ENV.DB.HOST,
-			port: +(ENV.DB.PORT || 5432),
-			username: ENV.DB.USERNAME,
-			password: ENV.DB.PASSWORD,
-			database: ENV.DB.NAME,
-			charset: 'utf8mb4',
-			synchronize: false, // This should always be false, if you want to add , then please do through migration
-			logging: false,
-			autoLoadEntities: true,
-		}),
-	CacheModule.register({
-		isGlobal: true, // Makes the cache available across the entire app
-		ttl: 0.5 * 60 * 1000, // Optional: Time-to-live (in milliseconds), default is 30 seconds
-		max: 100, // Optional: Maximum number of items in cache
-	}),
+    TypeOrmModule.forRoot({
+      type: ENV.DB.TYPE as any,
+      host: ENV.DB.HOST,
+      port: +(ENV.DB.PORT || 5432),
+      username: ENV.DB.USERNAME,
+      password: ENV.DB.PASSWORD,
+      database: ENV.DB.NAME,
+      charset: 'utf8mb4',
+      synchronize: false, // This should always be false, if you want to add , then please do through migration
+      logging: false,
+      autoLoadEntities: true,
+      // entities: ['dist/**/*.entity.js'],
+      // migrations: ['dist/migration/*.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    CacheModule.register({
+      isGlobal: true, // Makes the cache available across the entire app
+      ttl: 0.5 * 60 * 1000, // Optional: Time-to-live (in milliseconds), default is 30 seconds
+      max: 100, // Optional: Maximum number of items in cache
+    }),
     AdminModule,
     SharedModule,
     AuthModule,
-	CategoryModule,
-	UserModule,
-	BlogModule
+    CategoryModule,
+    UserModule,
+    BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
