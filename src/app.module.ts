b982b1a60143d4c5711d706758c168f4d9c28938
mAsadSiddique <asadsiddique.a@gmail.com>
2025-12-28 21:56:23 +0500

    test circle ci

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { ENV } from './config/constant'
-import { SharedModule } from './shared/shared.module'
-import { AuthModule } from './auth/auth.module'
-import { AdminModule } from './admin/admin.module'
-import { CacheModule } from '@nestjs/cache-manager'
-import { CategoryModule } from './category/category.module'
-import { UserModule } from './user/user.module'
-import { BlogModule } from './blog/blog.module'
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENV } from './config/constant';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
@@ -27,7 +27,7 @@ import { BlogModule } from './blog/blog.module'
     CacheModule.register({
       isGlobal: true, // Makes the cache available across the entire app
       ttl: 0.5 * 60 * 1000, // Optional: Time-to-live (in milliseconds), default is 30 seconds
-      max: 100, // Optional: Maximum number of items in cache
+      max: 100,
     }),
     AdminModule,
     SharedModule,
@@ -39,4 +39,4 @@ import { BlogModule } from './blog/blog.module'
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
